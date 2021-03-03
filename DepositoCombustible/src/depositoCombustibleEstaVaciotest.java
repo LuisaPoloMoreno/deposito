04d62a297d5f658cb6991355af2ed2db219f68a7
Luisa Fernanda Polo Moreno <luisita021194@gmail.com>
2021-03-03 12:02:40 +0100

    prueba del metodo esta vacio

---
+++ DepositoCombustible/src/depositoCombustibleEstaVaciotest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class depositoCombustibleEstaVaciotest {
+	
+	private DepositoCombustible DepositoCombustible ;
+
+	@Test
+	void testEstaVacio() {
+		DepositoCombustible dep= new DepositoCombustible (55.00, 22.00);
+		dep.consumir (22.00);
+		assertTrue (dep.estaVacio());
+	}
+
+	void testEstaVacio2() {
+		DepositoCombustible dep= new DepositoCombustible (55.00, 22.00);
+		dep.fill (15.00);
+		assertFalse (dep.estaVacio());
+	}
+}
